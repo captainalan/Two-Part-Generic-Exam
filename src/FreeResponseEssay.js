63a754ed2c0dba921480e2825b02de6ce7e4251b
Alan Wong <captainalan@gmail.com>
2018-10-20 18:33:04 -0700

    Making code organization more modular.

---
+++ src/FreeResponseEssay.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './ScoreBox.css';
+
+class FreeResponseEssay extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            initial_text: "I think that I'm clever because...",
+            essay_submitted: false,
+        };
+
+        this.state.value = this.state.initial_text;
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleClick(event) {
+        // Clear the original prompt text when user clicks text area
+        if (this.state.value === this.state.initial_text){
+            this.setState({value: ''});
+        }
+    }
+
+    handleBlur(event) {
+        if (this.state.value === ''){
+            this.setState({value: this.state.initial_text});
+        }
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.value); // Send essay up!
+        this.setState({ essay_submitted: true })
+    }
+
+    wordCount() {
+        return (this.state.value).split(" ").length;
+    }
+
+    render() {
+        return(
+            <div className='FreeResponeEssay'>
+                <h2>Here is the free response section.</h2>
+                <form onSubmit={this.handleSubmit}>
+                <label> {/* Figure out what <label /> is for */}
+                    <p>Here, you can write about your feelings. Please write at least a dozen words.</p>
+                    <textarea 
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        onClick={this.handleClick}
+                        onBlur={this.handleBlur}
+                        rows="10" 
+                        cols="79"
+                        id="essay" name="essay"
+                    />
+                    {/* Insert word count feature here. */}
+                    <p>Current word count is approximately {this.wordCount()} words.</p>
+                    {!this.state.essay_submitted ? 
+                        <p>Remember to click the Submit button to submit your essay!</p> 
+                        : null }
+                </label> <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                {this.state.essay_submitted ? 
+                    <p>Your essay has been submitted!</p>
+                    : null }
+
+
+            </div>
+        );
+    }
+}
+export default FreeResponseEssay;
---
+++ src/QuestionList.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './ScoreBox.css';
+
+class QuestionList extends React.Component {
+
+    choiceSelected(qid) {
+        // qid is something like 'q1'; returns first found response to 
+        // this question (e.g. 0, 1). Otherwise, returns null
+        if (this.props.responses && (qid in this.props.responses)) {
+            return this.props.responses[qid];
+        }
+        return null;
+    }
+    constructor(props) {
+        super(props);
+        this.choiceSelected = this.choiceSelected.bind(this);
+    }
+
+    render() {
+        const { questions } = this.props; // Maybe get responses props here too
+        return(
+            <div>
+                {questions.map((item,index) =>
+                    <Question 
+                        key={'q' + [index]} /* Look for a better solution than this */
+                        selected={this.choiceSelected('q' + [index])}
+                        question={item.question}
+                        choices={item.choices}
+                        choiceHandler={(choice) => this.props.selectChoice(index,choice)}
+                    />
+                )}
+            </div>
+        )
+    }
+}
+
+// Each multiple choice question consists of a problem statement and
+// answer choices.
+class Question extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.choiceHandler = this.choiceHandler.bind(this);
+    }
+
+    choiceHandler(i){ 
+        this.props.choiceHandler(i);
+    }
+
+    render() {
+        return (
+            <div className='Question'>
+                <Problem questionText={this.props.question}/>
+                {/* Insert code here to render choices*/}
+                {this.props.choices.map((choice_text, i) =>
+                    <Choice 
+                        key={choice_text} // Better than using indices?
+                        className={i === this.props.selected ? 'Selected' : 'Choice'}
+                        onClick={ () => this.choiceHandler(i) }
+                    >
+                        {choice_text}
+                    </Choice>
+                )}
+            </div>
+        )
+    }
+}
+
+// Statement of problem for multiple choice Question
+function Problem(props) {
+    return (<div><p>{props.questionText}</p></div>);
+}
+
+// Answer choice for multiple choice Question
+class Choice extends React.Component {
+
+    render() {
+        const {
+            className,
+            children,
+            onClick,
+        } = this.props;
+        return (
+            <button 
+                className={className}
+                type="button"  
+                onClick={onClick}
+            >
+                {children}
+            </button>     
+        );
+    }
+}
+
+export default QuestionList;
