5a8f72221b6761c474d1bb2d23dac5909cc66ead
Alan Wong <captainalan@gmail.com>
2018-12-06 15:51:59 -0800

    Updates

---
+++ precache-manifest.f6f820f83d00b9800fb3df40a68c7183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c85de6a4d66a7b133031",
+    "url": "/two-part-generic-exam/static/css/main.7d0b58e1.chunk.css"
+  },
+  {
+    "revision": "c85de6a4d66a7b133031",
+    "url": "/two-part-generic-exam/static/js/main.c85de6a4.chunk.js"
+  },
+  {
+    "revision": "c6c871c844a4b9ab7db3",
+    "url": "/two-part-generic-exam/static/js/1.c6c871c8.chunk.js"
+  },
+  {
+    "revision": "7c38ae448651d1b68d84",
+    "url": "/two-part-generic-exam/static/js/runtime~main.7c38ae44.js"
+  },
+  {
+    "revision": "68bba70da3da0c444a197e1fe1d5d93f",
+    "url": "/two-part-generic-exam/index.html"
+  }
+];
